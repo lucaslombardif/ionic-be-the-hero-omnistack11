3c50bbaaf467b2beb248331846ce7b25451a8367
lucaslombardif <lucaas-floriano@hotmail.com>
2020-04-08 15:49:36 -0300

    permitindo social sharing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ApiProvider } from '../providers/api/api';
 import { HttpClientModule } from '@angular/common/http';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { HttpClientModule } from '@angular/common/http';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    ApiProvider
+    ApiProvider,
+    SocialSharing
   ]
 })
 export class AppModule {}
--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
-/**
- * Generated class for the DetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 @IonicPage()
 @Component({
@@ -14,18 +9,47 @@ import { HomePage } from '../home/home';
   templateUrl: 'detail.html',
 })
 export class DetailPage {
-  
+
   public incident: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public socialSharing: SocialSharing
+  ) {
+
   }
 
   ionViewDidLoad() {
-     this.incident = this.navParams.data.incident;
-     console.log(this.incident);
+    this.incident = this.navParams.data.incident;
+    console.log(this.incident);
   }
 
-  backToHome() { 
+  sendWppMessage() {
+    let message = `Olá ${this.incident.name}, estou entrando em contato pois gostaria de ajudar no caso: "${this.incident.title}" com o valor de R$ ${this.incident.value};`
+
+    this.socialSharing.shareViaWhatsApp(message, null, null)
+      .then((sucess) => {
+        console.log('Mensagem enviada com sucesso!', sucess);
+      }).catch((err) => {
+        console.log(err);
+      });
+  }
+
+  sendEmailMessage() {
+    let message = `Olá ${this.incident.name}, estou entrando em contato pois gostaria de ajudar no caso: "${this.incident.title}" com o valor de R$ ${this.incident.value};`
+    this.socialSharing.shareViaEmail(
+      message,
+      `Herói do caso: ${this.incident.title}`,
+      [this.incident.email],
+    ).then((sucess) => {
+      console.log("Email enviado com sucesso!", sucess);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  backToHome() {
     this.navCtrl.push(HomePage);
   }
 
